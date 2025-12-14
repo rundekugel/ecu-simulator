ecc8dec3426d3595810b2535e85601eea2caaef1
rundekugel <rundekugel@gmx>
2025-12-14 02:12:12 +0100

    Added more services and fix for TOPDON OBD reader
    1. added response PID01, needed for TOPDON OBD reader
    2. added services 03,04,07,0A

--- ecu-simulator.py
+++ ecu-simulator.py
@@ -10,74 +10,133 @@ import getopt, sys
 import can
 from can.bus import BusState
 
-def service1(bus, msg):
-    if msg.data[2] == 0x00:
-        log.debug(">> Caps")
-        msg = can.Message(arbitration_id=0x7e8,
-          data=[0x06, 0x41, 0x00, 0xBF, 0xDF, 0xB9, 0x91],
+
+emu_ecu_can_id = 0x7e8
+
+class globs:
+    mil_on = 0
+    confirmed_DTCs = 2
+    dtcs = [0xc158, 0x0001]
+
+
+def cansend(bus, msg: can.message):
+    data =msg.data
+    leng = len(data)
+    if data[0] == 0:
+        l = leng -1
+        data[0] = l
+    if len(data)!=8:    # padding
+        data += b"\x00"*(8-len(data))
+        # data += b"\xcc"*(8-len(data))
+    bus.send(msg)
+
+def service1(bus, msg :can.message):
+    confirmed_DTCs = 2
+    pid = msg.data[2]
+    txmsg = can.Message(arbitration_id= emu_ecu_can_id,
+          data=[0x00, 0x41, pid],
           is_extended_id=False)
-        bus.send(msg)
-    elif msg.data[2] == 0x04:
+    # print(hex(msg.arbitration_id),  msg.data, pid)
+    if pid == 0x00:
+        log.debug(">> Supported PIDs")
+        # old: [0xBF, 0xDF, 0xB9, 0x91]
+        if 0:
+            p0= 0b10110000 # pids 1,3,4
+            # p0 = 0b00001011
+            p1, p2, p3 =0,0,0
+            pids = [p0,p1,p2,p3]    # big endian
+        pids = [0xBF, 0xDF, 0xB9, 0x91]
+        txmsg.data[3:] = pids
+        cansend(bus, txmsg)
+    elif pid == 0x01:   # added by rundekugel
+        log.debug(">> Status")
+        txmsg.data[3:]= bytes([ (globs.mil_on <<7)|globs.confirmed_DTCs, 0,0,0,0])
+        cansend(bus, txmsg)
+    elif pid == 0x04:
         log.debug(">> Calculated engine load")
-        msg = can.Message(arbitration_id=0x7e8,
-          data=[0x03, 0x41, 0x04, 0x20],
-          is_extended_id=False)
-        bus.send(msg)
-    elif msg.data[2] == 0x05:
+        txmsg.data[3:]= bytes([0x20])
+        cansend(bus, txmsg)
+    elif pid == 0x05:
         log.debug(">> Engine coolant temperature")
-        msg = can.Message(arbitration_id=0x7e8,
-          data=[0x03, 0x41, 0x05, randint(88 + 40, 95 + 40)],
-          is_extended_id=False)
-        bus.send(msg)
-    elif msg.data[2] == 0x0B:
+        txmsg.data[3:]= bytes([randint(88 + 40, 95 + 40)])
+        cansend(bus, txmsg)
+    elif pid == 0x0B:
         log.debug(">> Intake manifold absolute pressure")
-        msg = can.Message(arbitration_id=0x7e8,
-          data=[0x04, 0x41, 0x0B, randint(10, 40)],
-          is_extended_id=False)
-        bus.send(msg)
-    elif msg.data[2] == 0x0C:
+        txmsg.data[3:]= bytes([randint(10, 40)])
+        cansend(bus, txmsg)
+    elif pid == 0x0C:
         log.debug(">> RPM")
-        msg = can.Message(arbitration_id=0x7e8,
-          data=[0x04, 0x41, 0x0C, randint(18, 70), randint(0, 255)],
-          is_extended_id=False)
-        bus.send(msg)
-    elif msg.data[2] == 0x0D:
+        txmsg.data[3:]= bytes([ randint(18, 70), randint(0, 255)])
+        cansend(bus, txmsg)
+    elif pid == 0x0D:
         log.debug(">> Speed")
-        msg = can.Message(arbitration_id=0x7e8,
-          data=[0x03, 0x41, 0x0D, randint(40, 60)],
-          is_extended_id=False)
-        bus.send(msg)
-    elif msg.data[2] == 0x0F:
+        txmsg.data[3:]= bytes([ randint(40, 60)])
+        cansend(bus, txmsg)
+    elif pid == 0x0F:
         log.debug(">> Intake air temperature")
-        msg = can.Message(arbitration_id=0x7e8,
-          data=[0x03, 0x41, 0x0F, randint(60, 64)],
-          is_extended_id=False)
-        bus.send(msg)
-    elif msg.data[2] == 0x10:
+        txmsg.data[3:]= bytes([ randint(60, 64)])
+        cansend(bus, txmsg)
+    elif pid == 0x10:
         log.debug(">> MAF air flow rate")
-        msg = can.Message(arbitration_id=0x7e8,
-          data=[0x04, 0x41, 0x10, 0x00, 0xFA],
-          is_extended_id=False)
-        bus.send(msg)
-    elif msg.data[2] == 0x11:
+        txmsg.data[3:]= bytes([ 0x00, 0xFA])
+        cansend(bus, txmsg)
+    elif pid == 0x11:
         log.debug(">> Throttle position")
-        msg = can.Message(arbitration_id=0x7e8,
-          data=[0x03, 0x41, 0x11, randint(20, 60)],
-          is_extended_id=False)
-        bus.send(msg)
-    elif msg.data[2] == 0x33:
+        txmsg.data[3:]= bytes([ randint(20, 60)])
+        cansend(bus, txmsg)
+    elif pid == 0x33:
         log.debug(">> Absolute Barometric Pressure")
-        msg = can.Message(arbitration_id=0x7e8,
-          data=[0x03, 0x41, 0x33, randint(20, 60)],
-          is_extended_id=False)
-        bus.send(msg)
+        txmsg.data[3:]= bytes([ randint(20, 60)])
+        cansend(bus, txmsg)
     else:
-        log.warning("!!! Service 1, unknown code 0x%02x", msg.data[2])
+        log.warning("!!! Service 1, unknown code 0x%02x", pid)
+        txmsg.data[3:]= bytes([0x02, 0x7f, 0x22, 0x31])
+        cansend(bus, txmsg)
+
+def service3(bus, msg :can.message): 
+    """Show stored Diagnostic Trouble Codes (DTCs)"""
+    dtc2bytes = [0xc1, 0x15, 0x00, 0x01]
+    pid = msg.data[2]
+    print(hex(msg.arbitration_id),  msg.data, pid)
+    txmsg = can.Message(arbitration_id =emu_ecu_can_id,
+        data=[0x00, 0x43, globs.confirmed_DTCs ] +dtc2bytes ,
+        is_extended_id=False)    
+    cansend(bus, txmsg)
+
+def service7(bus, msg :can.message): 
+    """Show pending Diagnostic Trouble Codes (detected during current or last driving cycle) """
+    pid = msg.data[2]
+    print(hex(msg.arbitration_id),  msg.data, pid)
+    txmsg = can.Message(arbitration_id =emu_ecu_can_id,
+        data=[0x00, 0x47, 0, 0,0,0,0],
+        is_extended_id=False)    
+    cansend(bus, txmsg)
+
+def service10(bus, msg :can.message): 
+    """send Permanent Diagnostic Trouble Codes (DTCs) (Cleared DTCs) """
+    pid = msg.data[2]
+    print(hex(msg.arbitration_id),  msg.data, pid)
+    txmsg = can.Message(arbitration_id =emu_ecu_can_id,
+        data=[0x00, 0x4a, 0, 0,0,0,0],
+        is_extended_id=False)    
+    cansend(bus, txmsg)
+
+
+def service4(bus, msg :can.message):
+    """ Clear DTCs"""
+    log.debug(">> Clear DTCs")
+    pid = msg.data[2]
+    print(hex(msg.arbitration_id),  msg.data, pid)
+    txmsg = can.Message(arbitration_id =emu_ecu_can_id,
+        data=[0x00, 0x44, 0],
+        is_extended_id=False)    
+    cansend(bus, txmsg)
+    globs.confirmed_DTCs = 0
 
 
 def receive_all():
 
-    bus = can.interface.Bus(bustype='socketcan',channel='can0')
+    bus = can.interface.Bus(interface='socketcan',channel='can0')
     #bus = can.interface.Bus(bustype='ixxat', channel=0, bitrate=250000)
     #bus = can.interface.Bus(bustype='vector', app_name='CANalyzer', channel=0, bitrate=250000)
 
@@ -87,13 +146,33 @@ def receive_all():
     try:
         while True:
             msg = bus.recv(1)
-            if msg is not None:
+            while 1:
                 #print(msg)
-                if msg.arbitration_id == 0x7df and msg.data[1] == 0x01:
+                if msg is None:
+                    break
+                if msg.arbitration_id in [emu_ecu_can_id]:
+                    log.warning("Other ECU is also responding!")
+                    break
+                if not msg.arbitration_id in [0x7df, 0x7e0]:
+                    log.warning("Unknown ID %d (=0x%x) "%(msg.arbitration_id,msg.arbitration_id))
+                    break
+                if msg.data[1] == 0x01:
                     service1(bus, msg)
-                else:
-                    log.warning("Unknown ID %d or service code 0x%02x", msg.arbitration_id, msg.data[1])
-
+                    break
+                if msg.data[1] == 0x03:
+                    service3(bus, msg)
+                    break
+                if msg.data[1] == 0x04:
+                    service4(bus, msg)
+                    break
+                if msg.data[1] == 0x07:
+                    service7(bus, msg)
+                    break
+                if msg.data[1] == 0x0a:
+                    service10(bus, msg)
+                    break
+                log.warning("Unknown service code 0x%02x", msg.data[1])
+                break
     except KeyboardInterrupt:
         pass
 
